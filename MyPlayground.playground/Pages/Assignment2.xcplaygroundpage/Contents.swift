305bfc59c2c3d33059cf8094b2a70461909e9968
HiteshMadaan28 <hiteshmadaan28@gmail.com>
2024-01-30 21:31:18 +0530

    Done for day 3

--- MyPlayground.playground/Pages/Assignment2.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Assignment2.xcplaygroundpage/Contents.swift
@@ -1,7 +1,13 @@
-//: [Previous](@previous)
+import Cocoa
 
-import Foundation
+//create an array of string,then write some code that prints the number of items in the array and also the number of unique items in the array.
 
-var greeting = "Hello, playground"
+var arr:[String]=["1","oo","ee","oo","ee","1s","this"]
 
-//: [Next](@next)
+
+
+var set1:Set<String>=Set(arr)
+
+
+print("The Number of items in array \(arr.count) and the number of unique items in the array are \(set1.count)")
+
--- MyPlayground.playground/Pages/Day3.1.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Day3.1.xcplaygroundpage/Contents.swift
@@ -4,4 +4,20 @@ import Cocoa
 let surname:String="Madaan"
 var score:Int=99
 var marks:Double=0
+var isAuthenicated:Bool=true
 
+var ablums:[String]=["kgf","TNt"]
+var dic:[String:String]=["ss":"Ss"]
+
+var set:Set<String>=Set(["sd","dsf"])
+
+var a1:[String]=[String]()
+var a2:[String]=[]
+var a3=[String]()
+
+
+let username:String
+
+username="Hitesh"
+
+print(username)
