fa49de697e2a2eac42f003db574cfbf7017f91a6
HiteshMadaan28 <hiteshmadaan28@gmail.com>
2024-02-15 23:41:50 +0530

    commit

--- MyPlayground.playground/Pages/Day12.5.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Day12.5.xcplaygroundpage/Contents.swift
@@ -16,9 +16,15 @@ class User{
     }
 }
 
+
+var user=[User]()
 for i in 1...3{
     var u=User(id:i)
     print("I'am in control")
+    user.append(u)
 }
 
+print("Loop is finished")
+user.removeAll()
+print("Array is Cleared")
 
