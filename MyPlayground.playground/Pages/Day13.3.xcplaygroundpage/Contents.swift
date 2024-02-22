6e8f59644458c13fbd5bb9fa80a9807e0dd54216
HiteshMadaan28 <hiteshmadaan28@gmail.com>
2024-02-22 22:51:54 +0530

    working

--- MyPlayground.playground/Pages/Day13.3.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Day13.3.xcplaygroundpage/Contents.swift
@@ -10,4 +10,28 @@ extension String{
 
 var quote="   This is the quote  "
 
+print(quote.trimmed())
 
+
+struct Book{
+    var title:String
+    var price:Int
+    
+    
+}
+
+extension Book{
+    init(title: String) {
+        self.title = title
+        self.price = 30
+    }
+    
+    var Char:Int{
+        title.count
+    }
+}
+
+let b1=Book(title: "Harry porter", price: 3)
+var b2=Book(title: "Honey")
+
+print("\(b1.Char) and \(b2.Char)")
